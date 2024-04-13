36d98ad7fba1f096fc1f30c248081d5c0f627d39
Huiping27 <cathylhp@gmail.com>
2024-04-13 22:10:50 +0200

    weekday_avg_temp

--- models/mart/weekday_avg_temp.sql
+++ models/mart/weekday_avg_temp.sql
@@ -1,3 +1,17 @@
-select avg_temp_c
-,DATE_PART('week', date) AS week
-from prep_forecast_day pfd
+-- Define a DBT model to create a new table representing average temperature for weekdays
+with forecast_day_data as (
+    select * 
+    from {{ ref('staging_forecast_day') }}
+),
+add_features as (
+    select *
+        ,DATE_PART('day', date) as day_of_month -- day of month as a number
+        ,TO_CHAR(date, 'Month') as month_of_year -- month name as a text
+        ,DATE_PART('year', date) as year -- year as a number
+        ,TO_CHAR(date, 'Day') as day_of_week -- weekday name as text
+        ,DATE_PART('week', date) as week_of_year -- calender week number as number
+        ,TO_CHAR(date, 'IYYY-IW') as year_and_week -- year-calenderweek as text like '2024-43'
+    from forecast_day_data
+)
+select *
+from add_features
