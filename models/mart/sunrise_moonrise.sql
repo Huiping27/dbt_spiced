e354f2fa792dd893793b0a93cad11b6f2ec22c56
Huiping27 <cathylhp@gmail.com>
2024-04-15 08:04:31 +0200

    sunrise_moonrise

--- models/mart/sunrise_moonrise.sql
+++ models/mart/sunrise_moonrise.sql
@@ -1,17 +1,17 @@
 -- Define a DBT model to create a new table representing sunrise & moonrise for weekdays
 with forecast_day_data as (
-          select sunrise, moonrise 
-          from {{ ref('staging_forecast_day') }}
-      ),
-      add_features as (
-          select sunrise, moonrise,
-              DATE_PART('day', date) as day_of_month,
-              TO_CHAR(date, 'Month') as month_of_year,
-              DATE_PART('year', date) as year,
-              TO_CHAR(date, 'Day') as day_of_week,
-              DATE_PART('week', date) as week_of_year,
-              TO_CHAR(date, 'IYYY-IW') as year_and_week
-          from forecast_day_data
-      )
-      select *
-      from add_features
+    select sunrise, moonrise 
+    from "climate"."dbt_hli"."staging_forecast_day"
+),
+add_features as (
+    select sunrise, moonrise,
+        DATE_PART('day', forecast_date) as day_of_month,
+        TO_CHAR(forecast_date, 'Month') as month_of_year,
+        DATE_PART('year', forecast_date) as year,
+        TO_CHAR(forecast_date, 'Day') as day_of_week,
+        DATE_PART('week', forecast_date) as week_of_year,
+        TO_CHAR(forecast_date, 'IYYY-IW') as year_and_week
+    from forecast_day_data
+)
+select *
+from add_features;
