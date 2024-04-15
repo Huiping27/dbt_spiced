23be87a7b2cf55be5f831ca5e8f0f84ec3e09066
Huiping27 <cathylhp@gmail.com>
2024-04-15 14:20:10 +0200

    sunrise_moonrise

--- models/mart/sunrise_moonrise.sql
+++ models/mart/sunrise_moonrise.sql
@@ -1,17 +1,15 @@
 -- Define a DBT model to create a new table representing sunrise & moonrise for weekdays
 with forecast_day_data as (
     select sunrise, moonrise 
-    from {{ ref('staging_forecast_day') }}
+    from {{ ref('prep_forecast_day') }}
 ),
 add_features as (
-    select sunrise, moonrise,
-        ,DATE_PART('day', date) as day_of_month -- day of month as a number
-        ,TO_CHAR(date, 'Month') as month_of_year -- month name as a text
-        ,DATE_PART('year', date) as year -- year as a number
-        ,TO_CHAR(date, 'Day') as day_of_week -- weekday name as text
-        ,DATE_PART('week', date) as week_of_year -- calender week number as number
-        ,TO_CHAR(date, 'IYYY-IW') as year_and_week -- year-calenderweek as text like '2024-43'
-    from forecast_day_data
+    SELECT 
+	  (CASE WHEN moonrise = 'No moonrise' THEN null ELSE moonrise END)::TIME moonrise_n
+	  ,CAST((CASE WHEN moonrise = 'No moonrise' THEN null ELSE moonrise END)  AS TIME)
+    FROM prep_forecast_day
 )
 select *
 from add_features
+
+
